d3644edda61b73f94c569a786c3ea18a59e13b4a
swati garg <swat.garg@sourcefuse.com>
2022-07-31 12:46:38 +0530

    added

---
+++ src/behaviouralDesignPattern/strategy/registryMethod/Mode.java
@@ -0,0 +1,7 @@
+package behaviouralDesignPattern.strategy.registryMethod;
+
+public enum Mode {
+    Car,
+    Bike,
+    Walk
+}
--- src/behaviouralDesignPattern/strategy/registryMethod/PathCalculationStrategy.java
+++ src/behaviouralDesignPattern/strategy/registryMethod/PathCalculationStrategy.java
@@ -1,4 +1,4 @@
-package behaviouralDesignPattern.strategy;
+package behaviouralDesignPattern.strategy.registryMethod;
 
 public interface PathCalculationStrategy {
     void calculatePath(String from, String to);
